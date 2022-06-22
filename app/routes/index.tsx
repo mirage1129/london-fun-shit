44fe9428cfd917592255b53721c2f320dd6ae0dc
meraj <meraj@merajs-MacBook-Pro.local>
2022-06-22 17:16:12 +0100

    put header in divs so it's more responsive

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,12 +1,16 @@
 export default function IndexRoute() {
   return (
   <div>
-    <div class="text-9xl font-semibold text-white text-center rotate-30 pt-56 bg-yellow-300 pl-12">
-      <p>ADVENTURE</p>
-      <p>AWAITS</p>
+    <div class="text-9xl font-semibold text-white rotate-30 pt-56 bg-yellow-300 pl-12">
+      <div class="flex justify-center"> 
+        <p>ADVENTURE</p>
+      </div>
+      <div class="flex justify-center">
+        <p>AWAITS</p>
+      </div>
     </div>
 
-    <div className="z-20 bg-red-500 flex justify-center">
+    <div className="flex bg-red-500 justify-center">
       <button
         type="submit"
         className="z-10 py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
