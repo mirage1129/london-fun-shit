819c64a75a870fa07af3717d4e1f2911962fd848
meraj <you@example.com>
2022-06-23 16:29:23 +0100

    streamlined npm run dev to hot reload faster

--- app/routes/$adventures.tsx
+++ app/routes/$adventures.tsx
@@ -1,7 +1,7 @@
 export default function AdventuresRoute() {
   return (
     <div class="bg-lani-teal h-screen w-screen">
-     
+     <h1 class="text-3xl">testing testing 1 23 dfsfsfgfgdg</h1>
     </div>
   );
 }
