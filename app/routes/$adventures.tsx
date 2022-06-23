061bd8ad44b4f9187d80bf49a9bc0013af53ed1f
meraj <you@example.com>
2022-06-23 16:24:20 +0100

    added Adventures route

---
+++ app/routes/$adventures.tsx
@@ -0,0 +1,7 @@
+export default function AdventuresRoute() {
+  return (
+    <div class="bg-lani-teal h-screen w-screen">
+     
+    </div>
+  );
+}
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,30 +1,19 @@
 export default function IndexRoute() {
   return (
-  <div class="overflow-hidden bg-lani-teal h-screen w-screen">
-    <div class="text-6xl md:text-8xl lg:text-9xl font-semibold text-white rotate-30 bg-yellow-300 h-2/4 pl-12">
+  <div class="bg-lani-teal h-screen w-screen">
+    <div class="overflow-hidden text-6xl md:text-8xl lg:text-9xl font-semibold text-white rotate-30 bg-yellow-300 h-2/4 pl-12">
         <p class="absolute left-20 md:left-32 lg:left-80 xl:left-1/3 2xl:left-1/2 bottom-3 lg:bottom-6 xl:bottom-10 2xl:bottom-16">ADVENTURE AWAITS</p>
     </div>
     
-    <div className="flex bg-red-500 justify-center">
+    <div className="overflow-hidden flex bg-red-500 justify-center">
       <button
         type="submit"
         className="z-10 py-2 md:py-4 px-4 md:px-8 text-sm md:text-2xl border border-transparent shadow-sm font-medium rounded-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
       >
         surprise meh
       </button>
-    </div>
-
-
-
-
-
-
-  
+    </div>  
   </div>
-  
-
-
-
 
   );
 }
