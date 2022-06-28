7d5fbb93160ad3011d424857a843ffe08ac8526d
meraj <you@example.com>
2022-06-28 17:42:02 +0100

    testing KV store

--- app/routes/adventures/$adventureId.tsx
+++ app/routes/adventures/$adventureId.tsx
@@ -1,52 +1,41 @@
-import type { LoaderFunction } from "@remix-run/cloudflare"
-import { json } from "@remix-run/cloudflare"
-import { useLoaderData } from "@remix-run/react";
+// import type { LoaderFunction } from "@remix-run/cloudflare"
+// import { json } from "@remix-run/cloudflare"
+// import { useLoaderData } from "@remix-run/react";
 
-export const loader: LoaderFunction = async ({
-  params,
-}) => {
-  const res = await fetch(`https://rickandmortyapi.com/api/character/${params.adventureId}`)
-  return json(await res.json());
-    }
-
-export default function AdventuresRoute() {
-  const adventure = useLoaderData();
-  console.log(adventure);
-  return (
-    <div>
-      <p>{adventure.name} Adventure you know</p>
-    </div>
-  );
-}
-
-// export default function GistsRoute() {
-//   const gists = useLoaderData();
-//   return (
-//     <ul>
-//       {gists.map((gist) => (
-//         <li key={gist.id}>
-//           <a href={gist.html_url}>{gist.id}</a>
-//         </li>
-//       ))}
-//     </ul>
-//   );
-// }
-
-
-
-// export async function onRequestGet({ params }) {
-//   const res = await fetch(`https://rickandmortyapi.com/api/character/${params.id}`);
-//   const data = await res.json();
-//   const info = JSON.stringify(data, null, 2);
-//   return new Response(info);
-// }
-
-
+// export const loader: LoaderFunction = async ({
+//   params,
+// }) => {
+//   const res = await fetch(`https://rickandmortyapi.com/api/character/${params.adventureId}`)
+//   return json(await res.json());
+//     }
 
 // export default function AdventuresRoute() {
+//   const adventure = useLoaderData();
+//   console.log(adventure);
 //   return (
-//     <div class="bg-lani-teal h-screen w-screen">
-//      <h1 class="text-3xl">testing testing 1 23 dfsfsfgfgdg</h1>
+//     <div>
+//       <p>{adventure.name} Adventure you know</p>
 //     </div>
 //   );
 // }
+
+
+import {json, LoaderFunction} from "@remix-run/cloudflare";
+
+export const loader: LoaderFunction = async ({context}) => {
+  console.log(JSON.stringify(context)); // Displays `{"MYNAMESPACE":{},"ASSETS":{}}`
+  // const value = await context.ADVENTURE_INFO.get("2");
+  const value = await context.ADVENTURE_INFO.list();
+  console.log(value); // Displays `null`
+  return json({value});
+};
+
+export default function AdventuresRoute() {
+    const adventure = useLoaderData();
+    console.log(adventure);
+    return (
+      <div>
+        <p>{adventure} Adventure you know</p>
+      </div>
+    );
+  }
