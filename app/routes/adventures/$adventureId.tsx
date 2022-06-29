1dfb7251cdae50c8470a2afa04900f60f6fcfe30
meraj <you@example.com>
2022-06-29 13:13:45 +0100

    parsing the string and then accessing location

--- app/routes/adventures/$adventureId.tsx
+++ app/routes/adventures/$adventureId.tsx
@@ -28,12 +28,13 @@ export const loader: LoaderFunction = async ({context}) => {
 
 export default function AdventuresRoute() {
     const adventure = useLoaderData();
+    const adventureObject = JSON.parse(adventure.adventure)
 
    
     // console.log(adventure.name);
     return (
       <div>
-        {adventure.adventure.location}
+        {adventureObject.location}
         </div>
     );
   }
