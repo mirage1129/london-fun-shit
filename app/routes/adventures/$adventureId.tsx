766006f25cd61a9cce181b1174cd1f9c1daf4c7b
meraj <you@example.com>
2022-06-29 14:18:24 +0100

    removed params entirely

--- app/routes/adventures/$adventureId.tsx
+++ app/routes/adventures/$adventureId.tsx
@@ -18,7 +18,7 @@ import {json, LoaderFunction} from "@remix-run/cloudflare";
 //   );
 // }
 
-export const loader: LoaderFunction = async ({context}, {params}) => {
+export const loader: LoaderFunction = async ({context}) => {
   // console.log(JSON.stringify(context)); // Displays `{"MYNAMESPACE":{},"ASSETS":{}}`
 
   // const {
@@ -31,7 +31,7 @@ export const loader: LoaderFunction = async ({context}, {params}) => {
   // } = context;
 
 
-  const adventure = await context.ADVENTURE_INFO.get(`${params.adventureId}`);
+  const adventure = await context.ADVENTURE_INFO.get(`${context.adventureId}`);
   // const value = await context.ADVENTURE_INFO.list();
   // console.log(value); // Displays `null`
   return json({adventure});
