d2f82fde3465258d4ace74717206595d36760783
meraj <you@example.com>
2022-06-29 16:58:50 +0100

    removed commented lines

--- app/routes/adventures/$adventureId.tsx
+++ app/routes/adventures/$adventureId.tsx
@@ -1,39 +1,9 @@
 import { useLoaderData } from "@remix-run/react";
 import {json, LoaderFunction} from "@remix-run/cloudflare";
 
-// export const loader: LoaderFunction = async ({
-//   params,
-// }) => {
-  // const res = await fetch(`https://rickandmortyapi.com/api/character/${params.adventureId}`)
-//   return json(await res.json());
-//     }
-
-// export default function AdventuresRoute() {
-//   const adventure = useLoaderData();
-//   console.log(adventure);
-//   return (
-//     <div>
-//       <p>{adventure.name} Adventure you know</p>
-//     </div>
-//   );
-// }
-
 export const loader: LoaderFunction = async ({context, params}) => {
-  // console.log(JSON.stringify(context)); // Displays `{"MYNAMESPACE":{},"ASSETS":{}}`
-
-  // const {
-  //   request, // same as existing Worker API
-  //   env, // same as existing Worker API
-  //   params, // if filename includes [id] or [[path]]
-  //   waitUntil, // same as ctx.waitUntil in existing Worker API
-  //   next, // used for middleware or to fetch assets
-  //   data, // arbitrary space for passing data between middlewares
-  // } = context;
-  // add
 
   const adventure = await context.ADVENTURE_INFO.get(`${params.adventureId}`);
-  // const value = await context.ADVENTURE_INFO.list();
-  // console.log(value); // Displays `null`
   return json({adventure});
 };
 
@@ -41,17 +11,9 @@ export default function AdventuresRoute() {
     const adventure = useLoaderData();
     const adventureObject = JSON.parse(adventure.adventure)
 
-   
-    // console.log(adventure.name);
     return (
-      
-     
       <div>
         {adventureObject.location}
-        {/* {JSON.stringify(adventure)} */}
-        </div>
-
-      // <div>dfsfsdf</div>
-
+      </div>
     );
   }
