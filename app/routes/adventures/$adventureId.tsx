cb2b1e193ffa2bcc523d57e67f001edf2b1bf9f4
meraj <you@example.com>
2022-07-08 16:47:02 +0100

    triggering deployment

--- app/routes/adventures/$adventureId.tsx
+++ app/routes/adventures/$adventureId.tsx
@@ -1,26 +1,19 @@
 import { useLoaderData } from "@remix-run/react";
 import {json, LoaderFunction} from "@remix-run/cloudflare";
 
-// export const loader: LoaderFunction = async ({context, params}) => {
-//   const adventure = await context.ADVENTURE_INFO.get(`${params.adventureId}`);
-//   return json({adventure});
-// };
+export const loader: LoaderFunction = async ({context, params}) => {
+  const adventure = await context.ADVENTURE_INFO.get(`${params.adventureId}`);
+  return json({adventure});
+};
 
 export default function AdventuresRoute() {
-    // const adventure = useLoaderData();
-    // const adventureObject = JSON.parse(adventure.adventure)
+    const adventure = useLoaderData();
+    const adventureObject = JSON.parse(adventure.adventure)
 
-    const testObject = {
-      name:"yolo",
-      location: "bobo"
-    }
 
     return (
       <div>
-        {/* {adventureObject.name}
-        {adventureObject.location} */}
-
-        {testObject.name}
+        {adventureObject.markup}
       </div>
     );
   }
